e56ebba3f65fa19be503e44448e95ff031a9a265
Rohit Waghode <rohitwaghode67@gmail.com>
2024-11-26 16:49:16 +0530

    error changes

--- frontend/src/Components/Signup/Signup.js
+++ frontend/src/Components/Signup/Signup.js
@@ -42,7 +42,7 @@ const Signup = () => {
         navigate("/");
       }
     } catch (error) {
-      console.log("Error in form submission:");
+      console.log("Error in form submission:", error);
     }
   };
 
